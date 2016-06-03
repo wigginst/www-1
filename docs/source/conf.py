7569f9736501eeb3eeff05cc068ec24a335dda24
li526 <li526@indiana.edu>
2016-06-03 13:53:25 -0400

    comment line 18

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@
 import sys
 import os
 #import sphinx_bootstrap_theme
-import pietroalbini_sphinx_themes
+#import pietroalbini_sphinx_themes
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
